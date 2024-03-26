a4762392d4b3e291a13848ab6de582c5e6143905
JunWoo Lee <junwoo1357@gmail.com>
2024-03-26 22:36:10 +0900

    css 입히기

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,7 +1,56 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import React from "react";
+import { Outlet } from "react-router-dom";
+import "./AppLayout.style.css";
 
 const AppLayout = () => {
-  return <div>AppLayout</div>;
+  return (
+    <div>
+      <Navbar
+        bg="dark"
+        data-bs-theme="dark"
+        expand="lg"
+        className="bg-body-tertiary"
+      >
+        <Container fluid>
+          <Navbar.Brand href="/" className="nav-brand-custom">
+            NETFLIX
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/" className="nav-link-custom">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/movies" className="nav-link-custom">
+                Movies
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex align-items-center">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2 search-input"
+                aria-label="Search"
+              />
+              <Button className="search-button" variant="outline-danger">
+                Search
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
 };
 
 export default AppLayout;
