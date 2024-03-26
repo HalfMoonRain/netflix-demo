628e3bafec29b91cba933787d72e5bbeae8e5d76
JunWoo Lee <junwoo1357@gmail.com>
2024-03-26 22:36:00 +0900

    notfound 페이지

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NotFoundPage.style.css";
+
+const NotFoundPage = () => {
+  return (
+    <div className="NotFound">
+      {/* Content can be added here if necessary */}
+    </div>
+  );
+};
+
+export default NotFoundPage;
